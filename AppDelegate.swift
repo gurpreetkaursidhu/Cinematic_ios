20c90950c74f2ec2f189113d03ade6dbdb90ef14
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-15 15:28:49 -0500

    Added Data source folder and View Models folder

---
+++ AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Cinematic
+//
+//  Created by Gurpreet Kaur on 2022-12-15.
+//
+
+import UIKit
+import CoreData
+class AppDelegate: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
