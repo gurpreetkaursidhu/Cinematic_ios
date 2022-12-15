b1bde55e983580f536cf8b03e84bb8cb6a2a62a4
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-14 22:48:20 -0500

    Added Constants

---
+++ Cinematic/Services/Service.swift
@@ -0,0 +1,26 @@
+//
+//  Service.swift
+//  Cinematic
+//
+//  Created by Gurpreet Kaur on 2022-12-14.
+//
+
+import Foundation
+
+
+
+class Service {
+
+    // MARK: - Utility
+    
+    func createApiUrl(with path:String, queryItems: [URLQueryItem]) -> URL? {
+        var query = [
+            URLQueryItem(name: "api_key", value: Constants.api().key),
+            URLQueryItem(name: "language", value: Constants.api().language)
+        ]
+        query.append(contentsOf: queryItems)
+        var urlComponents = URLComponents(string: "\(Constants.api().url)\(path)")
+        urlComponents?.queryItems = query
+        return urlComponents?.url
+    }
+}
