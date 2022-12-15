e1773467eb090add540000c7038becce03ab11f4
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-15 15:31:06 -0500

    App Delegate added

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -7,14 +7,46 @@
 
 import UIKit
 import CoreData
-class AppDelegate: UIView {
+class AppDelegate: UIResponder, UIApplicationDelegate  {
+    var window: UIWindow?
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
+
+        return true
     }
-    */
 
+    // MARK: - Core Data stack
+
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "the_movie_db")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                print(error)
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                print(nserror)
+            }
+        }
+    }
 }
+
+
+    
+
+
