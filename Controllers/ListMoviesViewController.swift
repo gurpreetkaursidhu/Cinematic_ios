923fd306596ca0ae6de8c77b82da8247acdeea26
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-15 16:58:10 -0500

    code for fun set up

--- Controllers/ListMoviesViewController.swift
+++ Controllers/ListMoviesViewController.swift
@@ -9,22 +9,29 @@ import UIKit
 
 class ListMoviesViewController: UIViewController ,MoviesViewInteractionLogic {
     private var viewModel = ListMoviesViewModel()
-   
+    
     
     // MARK: - Object lifecycle
-
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         title = viewModel.viewTitle
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         title = viewModel.viewTitle
     }
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
     // MARK: - Setup
-
+    private func setup() {
+        moviesView.viewController = self
+        
+        
+        
+    }
 }
-
