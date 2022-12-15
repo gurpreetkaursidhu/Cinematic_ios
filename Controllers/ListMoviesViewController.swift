77c68d6386eada87f4febddd6a4de10b95bfa8ba
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-15 16:45:22 -0500

    List movies view controller added

---
+++ Controllers/ListMoviesViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ListMoviesViewController.swift
+//  Cinematic
+//
+//  Created by Gurpreet Kaur on 2022-12-15.
+//
+
+import UIKit
+
+class ListMoviesViewController: UIViewController ,MoviesViewInteractionLogic {
+   
