263e6b9c11c60e753b58e785c3c5d22346523f86
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-15 00:22:36 -0500

    Movie service Added in services folder

---
+++ Cinematic/Views/MovieCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  MovieCollectionViewCell.swift
+//  Cinematic
+//
+//  Created by Gurpreet Kaur on 2022-12-14.
+//
+
+import Foundation
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var yearLabel: UILabel!
+    @IBOutlet private weak var posterImage: CustomImageView!
+
+    // MARK: - Object lifecycle
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel.sizeToFit()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        titleLabel.numberOfLines = 2
+    }
+
+    // MARK: - Setup
+
+    func setupCell(with movie: Movie) {
+        titleLabel.text = movie.title
+        yearLabel.text = movie.releaseYear
+        posterImage.loadImageUsing(path: movie.smallPosterPath)
+    }
+}
