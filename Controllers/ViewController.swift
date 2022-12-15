16ebc9671f16334b3c4dbf512370b8442d51660f
gurpreetkaur <A00252915@mycambrian.ca>
2022-12-15 15:33:29 -0500

    Controller Folder added

---
+++ Controllers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Cinematic
+//
+//  Created by Gurpreet Kaur on 2022-12-15.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
